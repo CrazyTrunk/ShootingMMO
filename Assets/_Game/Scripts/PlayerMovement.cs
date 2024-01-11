e7eb570afda3f125a753d5ff02791ffa200ce384
Crazy Trunk <ncongtrung1610@gmail.com>
2024-01-12 01:47:59 +0700

    Blend Tree

--- Assets/_Game/Scripts/PlayerMovement.cs
+++ Assets/_Game/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed = 3.5f;
     [SerializeField] private float rotateSpeed= 100f;
     [SerializeField] private Rigidbody rb;
+    [SerializeField] private Animator anim;
 
     private void FixedUpdate()
     {
@@ -17,6 +18,8 @@ public class PlayerMovement : MonoBehaviour
             rb.MoveRotation(rb.rotation * Quaternion.Euler(rotateY));
         }
         rb.MovePosition(rb.position + Input.GetAxis("Vertical") * moveSpeed * Time.fixedDeltaTime * transform.forward + Input.GetAxis("Horizontal") * moveSpeed * Time.fixedDeltaTime * transform.right);
+        anim.SetFloat("BlendV", Input.GetAxis("Vertical"));
+        anim.SetFloat("BlendH", Input.GetAxis("Horizontal"));
 
     }
 }
