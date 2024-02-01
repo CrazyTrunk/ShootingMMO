f5dcf63abf83b75a364417bb78468cd7128f8139
Crazy Trunk <ncongtrung1610@gmail.com>
2024-02-02 04:40:07 +0700

    Finish Switch Weapons

--- Assets/_Game/Scripts/WeaponChangerAdvanced.cs
+++ Assets/_Game/Scripts/WeaponChangerAdvanced.cs
@@ -5,6 +5,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations.Rigging;
+using UnityEngine.UI;
 
 public class WeaponChangerAdvanced : MonoBehaviour
 {
@@ -16,6 +17,12 @@ public class WeaponChangerAdvanced : MonoBehaviour
     [SerializeField] private GameObject[] weapons;
     [SerializeField] private PhotonView photonView;
 
+    [Header("UI for Weapon Switch")]
+    [SerializeField] private Sprite[] weaponIcons;
+    [SerializeField] private int[] ammoAmounts;
+
+    private Image weaponIcon;
+    private Text ammoAmountText;
 
     private int weaponNumber = 0;
     // Update is called once per frame
@@ -24,9 +31,14 @@ public class WeaponChangerAdvanced : MonoBehaviour
         if (Input.GetMouseButtonDown(1) && photonView.IsMine)
         {
             //weaponNumber++;
+            weaponIcon = GameObject.Find("WeaponUI").GetComponent<Image>();
+            ammoAmountText = GameObject.Find("AmmoText").GetComponent<Text>();
             photonView.RPC(nameof(ChangeWeapon), RpcTarget.AllBuffered);
+            //switch weapon when clicking mouse neu ma lon hon so luong weapon => reset ve 0
             if (weaponNumber > weapons.Length - 1)
             {
+                weaponIcon.GetComponent<Image>().sprite = weaponIcons[0];
+                ammoAmountText.text = ammoAmounts[0].ToString();
                 weaponNumber = 0;
             }
             for (int i = 0; i < weapons.Length; i++)
@@ -34,6 +46,8 @@ public class WeaponChangerAdvanced : MonoBehaviour
                 weapons[i].SetActive(false);
             }
             weapons[weaponNumber].SetActive(true);
+            weaponIcon.GetComponent<Image>().sprite = weaponIcons[weaponNumber];
+            ammoAmountText.text = ammoAmounts[weaponNumber].ToString();
             leftHand.data.target = leftTargets[weaponNumber];
             rightHand.data.target = rightTargers[weaponNumber];
             rig.Build();
